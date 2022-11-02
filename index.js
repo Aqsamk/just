9b91015da67264710e18f12b34fbc3a278196dd7
Aqsamk <aqsam.khan1999@gmail.com>
2022-11-02 17:20:14 +0530

    Getelementbytag

---
+++ index.js
@@ -0,0 +1,76 @@
+class User {
+    static countUsrs = 0
+
+    constructor (username,email,password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        User.countUsrs += 1; 
+    }
+    static countUsers(){
+        console.log(`Number of registered user are ${User.countUsrs}`);
+    }
+
+    register(){
+        console.log(this.username + ' is now registered.');
+
+    }
+
+}
+class Member extends User {
+    constructor(username,email,password,memberpackage){
+        super(username,email,password);
+        this.memberpackage = memberpackage;
+        let todayDate = new Date();
+        const memberahipactivtillyear = todayDate.getFullYear();
+        const memberahipactivtillmonth = todayDate.getMonth();
+        const memberahipactivtillday = todayDate.getDay();
+        this.memberahipactivtilldate = new Date(
+            memberahipactivtillyear,
+            memberahipactivtillmonth + 1,
+            memberahipactivtillday
+        );
+
+    }
+    renewmembership(){
+        const memberahipactivtillyear = this.memberahipactivtilldate.getFullYear();
+        const memberahipactivtillmonth = this.memberahipactivtilldate.getMonth();
+        const memberahipactivtillday = this.memberahipactivtilldate.getDay();
+
+        if(this.memberpackage === "standerd package"){
+        this.memberahipactivtilldate = new Date(
+            memberahipactivtillyear,
+            memberahipactivtillmonth + 1,
+            memberahipactivtillday
+        );
+        }else if (this.memberpackage === "yearly package"){
+            this.memberahipactivtilldate = new Date(
+                memberahipactivtillyear + 1,
+                memberahipactivtillmonth,
+                memberahipactivtillday
+            );
+        }
+    }
+
+    subscribtionActivetill(){
+        
+            console.log('The package for ' +  this.username + '  is active till '+ this.memberahipactivtilldate);
+       
+    }
+
+
+    getPackage(){
+        console.log(this.username + ' is subscribed to the '+ this.memberpackage + ' Package');
+    }
+}
+let U1 = new Member ('Aqsam','aqsam@gmail.com','12345','yearly package');
+let U2 = new Member ('Ved','ved@gmail.com',12344,'standerd package');
+U2.getPackage();
+U1.getPackage();
+U1.renewmembership();
+U2.renewmembership();
+U2.subscribtionActivetill();
+U1.subscribtionActivetill();
+U1.renewmembership();
+Member.countUsers();
+
--- main.js
+++ main.js
@@ -14,7 +14,7 @@ console.log(document.all[10]);
 console.log(document.forms[0]);
 console.log(document.links);*/
 
-console.log(document.getElementById('header-title'));
+/*console.log(document.getElementById('header-title'));
 var headerTitle = document.getElementById('header-title');
 var header = document.getElementById('main-header');
 headerTitle.textContent = ' hello';
@@ -24,5 +24,40 @@ headerTitle.innerHTML = '<h3>Hello</h3>';
 headerTitle.style.borderBottom = 'solid 3px red';
 header.style.borderBottom = 'solid 4px blue';
 var title = document.getElementsByClassName("title");
-title[0].className
+title[0].className*/
+
+//getElementbyclassname
+
+/*var items = document.getElementsByClassName('list-group-item');
+
+console.log(items);
+
+console.log(items[1]);
+
+//items[1].textContent = 'Hello 2';
+
+items[3].style.fontWeight = 'bold';
+
+items[2].style.backgroundColor = 'green';
+
+for(let i = 0;i<items.length;i++){
+
+    items[i].style.fontWeight = 'bold';
+
+}*/
+var li = document.getElementsByTagName('li');
+console.log(li);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'Pink';
+
+for(var i = 0;i<li.length;i++){
+    li[i].style.backgroundColor = 'pink';
+}
+
+//Quary selector
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px orange'
+
+
 
