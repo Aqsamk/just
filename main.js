0eb654d7821c34bcf171d9603969c3f63127d90f
Aqsamk <aqsam.khan1999@gmail.com>
2022-11-04 23:34:28 +0530

    using nodes

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ thirdItem.style.color = 'white';*/
 
 //quaryselectortoall
 
-var titles = document.querySelectorAll('.title');
+/*var titles = document.querySelectorAll('.title');
 console.log(titles);
 titles[0].textContent = 'Hello';
 
@@ -37,11 +37,79 @@ for(var i =0;i<even.length;i++){
     even[i].style.backgroundColor = 'orange';
     console.log('Nothing')
 
-}
+}*/
 
 //In quaryselector we can select only one thing at a time first item will be selected by default but 
 //in quaryselectorall we can do anything we can select anything by our choice.
+//Traversing the DOM
+
+var itemList = document.querySelector('#items');
+//parent node
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = 'violet';
+console.log(itemList.parentNode.parentNode.parentNode);
+
+console.log(itemList.parentElement);
+
+//child node
+console.log(itemList.childNodes);
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
+itemList.children[2].style.backgroundColor = 'green';
+
+//firstchild
+
+console.log(itemList.firstChild);
+console.log(itemList.firstElementChild);
+//itemList.firstElementChild.textContent = 'Hello Fools';
+itemList.lastElementChild.textContent = 'Last child';
+itemList.lastElementChild.style.backgroundColor ='pink';
+
+//next sibling
+
+console.log(itemList.nextSibling);
+console.log(itemList.nextElementSibling);
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.textContent = 'Aqsam';
+itemList.previousElementSibling.style.color = 'RED';
+
+var heading = itemList.parentNode.parentNode.parentNode.previousElementSibling;
+console.log(heading);
+
+console.log(heading.lastElementChild)
+heading.lastElementChild.textContent = 'Hello world Item Lister';
+
+console.log(itemList.firstElementChild);
+
+//create div
+var newDiv = document.createElement('div');
+newDiv.className = 'Hello';
+//add ID
+newDiv.id = 'Hello 1';
+
+//add atribute
+newDiv.setAttribute('title','Hello Div');
+
+//creaate text node
+var newDivText = document.createTextNode('Hello world');
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize = '30px'
+container.insertBefore(newDiv,h1);
+console.log(itemList.parentNode);
+//text node
+var item = document.getElementById('items');
+var parentnode = document.getElementById('Helloworld ');
+var Helloworld = document.createElement('li');
+Helloworld.appendChild(document.createTextNode(items));
+parentnode.appendChild(Helloworld);
 
 
+
+ 
 
 
