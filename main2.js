97814e122afbd4fb7ae28323b561053689e81dd3
Aqsamk <aqsam.khan1999@gmail.com>
2022-11-05 04:41:12 +0530

    object to string using jason stringify

--- main2.js
+++ main2.js
@@ -1,14 +1,22 @@
+
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
+
 myForm.addEventListener('submit',onSubmit);
 
 function onSubmit(e){
     e.preventDefault();
-    localStorage.setItem('name',nameInput.value);
-    localStorage.setItem('email',emailInput.value);
+    var nm = nameInput.value;
+    var mail = emailInput.value;
+    const obj = {
+       nm,
+       mail
+    }
+    localStorage.setItem('user Details',JSON.stringify(obj));
 }
+
 
