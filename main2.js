f7d5bf3ef605cb5fbe3320b0202e3b214be34c70
Aqsamk <aqsam.khan1999@gmail.com>
2022-11-13 11:03:44 +0530

    delete and edit

--- main2.js
+++ main2.js
@@ -1,32 +1,80 @@
-
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
+const myform = document.querySelector('#my-form');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
 const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
-
-
-myForm.addEventListener('submit',onSubmit);
+const users = document.querySelector('#users');
+myform.addEventListener('submit',onSubmit);
 
 function onSubmit(e){
     e.preventDefault();
-    var name = nameInput.value;
-    var mail = emailInput.value;
     
-    const obj = {
-       name,
-       mail
+       var nm = name.value;
+       var mail = email.value;
+
+    let obj = {
+        nm,
+        mail
     }
     localStorage.setItem(obj.mail,JSON.stringify(obj));
+    showNewUserOnScreen(obj);
+    }
+    window.addEventListener("DOMContentLoaded", () => {
+        const localStorageObj = localStorage;
+        const localstoragekeys  = Object.keys(localStorageObj)
 
-    if(name === '' || mail === ''){
-        msg.classList.add('error');
-    }else {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${name} : ${mail}` ));
-        userList.appendChild(li);
-        name ='';
-        mail='';
+        for(var i =0; i< localstoragekeys.length; i++){
+            const key = localstoragekeys[i]
+            const userDetailsString = localStorageObj[key];
+            const userDetailsObj = JSON.parse(userDetailsString);
+            showNewUserOnScreen(userDetailsObj)
+        }
+    })
+
+    function showNewUserOnScreen(user){
+        document.getElementById('email').value = '';
+                document.getElementById('name').value = '';
+                
+                // console.log(localStorage.getItem(user.emailId))
+                if(localStorage.getItem(user.mail) !== null){
+                    removeUserFromScreen(user.mail)
+                }
+        const parentNode = document.getElementById('users');
+        const childHTML = `<li id=${user.mail}> ${user.nm} - ${user.mail}
+                                <button onclick=deleteUser('${user.mail}')> Delete </button>
+                            
+                             <button onclick=editUserDetails('${user.mail}','${user.nm}')>Edit  </button>
+                             </li>`
+        parentNode.innerHTML = parentNode.innerHTML + childHTML;
     }
 
-}
+    // deleteUser('abc@gmail.com')
+
+    
+    function editUserDetails(mail, nm){
+
+        document.getElementById('email').value = mail;
+        document.getElementById('username').value = nm;
+        
+
+        deleteUser(mail)
+     }
+    
+    function deleteUser(mail){
+        console.log(mail)
+        localStorage.removeItem(mail);
+        removeUserFromScreen(mail);
+
+    }
+
+    function removeUserFromScreen(mail){
+        const parentNode = document.getElementById('users');
+        const childNodeToBeDeleted = document.getElementById(mail);
+
+        parentNode.removeChild(childNodeToBeDeleted)
+    }
+    
+
+       
+       
+    
+    
