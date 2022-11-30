f960fba3ada98883dd0a7c70864fae2a017eb99b
Aqsamk <aqsam.khan1999@gmail.com>
2022-11-30 17:22:18 +0530

    Apply call bind

---
+++ index3.js
@@ -0,0 +1,26 @@
+var obj = {num:2};
+var obj2 = {num:5};
+var addTothis = function(a,b,c){
+    return this.num+a+b+c;
+};
+//call
+let c = addTothis.call(obj,3,4,5); //functionname.call(obj,functionargument)
+console.log(c);
+//apply
+var arr = [1,2,3];
+console.log(addTothis.apply(obj,arr));
+console.log(addTothis.apply(obj2,arr));
+//bind
+var bound = addTothis.bind(obj);
+console.dir(bound);
+
+console.log(bound(1,2,3))
+
+
+//function for student
+var Student = {age:20};
+var ageOfStudent = function(){
+    return this.age;
+}
+var bound2 = ageOfStudent.bind(Student);
+console.log(bound2());
