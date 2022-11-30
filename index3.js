313c48a12859c3adf734ef988ebf2cfab7f1ecc0
Aqsamk <aqsam.khan1999@gmail.com>
2022-11-30 17:40:07 +0530

    currying

--- index3.js
+++ index3.js
@@ -1,26 +1,18 @@
-var obj = {num:2};
-var obj2 = {num:5};
-var addTothis = function(a,b,c){
-    return this.num+a+b+c;
-};
-//call
-let c = addTothis.call(obj,3,4,5); //functionname.call(obj,functionargument)
-console.log(c);
-//apply
-var arr = [1,2,3];
-console.log(addTothis.apply(obj,arr));
-console.log(addTothis.apply(obj2,arr));
-//bind
-var bound = addTothis.bind(obj);
-console.dir(bound);
+//let multiply = function(x,y){
+ //   console.log(x*y)
+//}
 
-console.log(bound(1,2,3))
-
-
-//function for student using bind
-var Student = {age:20};
-var ageOfStudent = function(){
-    return this.age;
+let multiply = function(x){
+    return function(y){
+        console.log(x*y);
+    }
 }
-var bound2 = ageOfStudent.bind(Student);
-console.log(bound2());
+let multiplybyTwo = multiply(2);
+ multiplybyTwo(5);
+
+//let multiplybyThree = multiply.bind(this,3);
+//multiplybyThree(6);
+
+let multiplybyThree = multiply(3);
+multiplybyThree(3);
+
