d0bd4df38ff673757620b576d047f15d84f7b9b4
Aqsamk <aqsam.khan1999@gmail.com>
2023-01-19 00:17:03 +0530

    body parsing with express js

--- app1.js
+++ app1.js
@@ -1,17 +1,24 @@
 const http = require('http');
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((rqs,res,next) => {
-  console.log('in the middleware!')
-  next();
-})
-app.use((req,res,next) => {
-  console.log('in the another middleware!')
-  res.send('<h1>Hello</h1>')
-})
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next) => {
  
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add product</button></form>')
+})
+
+app.post('/product',(req,res,next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
+app.use('/',(req,res,next) => {
+  
+  res.send('<h1>Hello from the express</h1>')
+})
   
 app.listen(3000);
